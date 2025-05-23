f9507bc5d28d72c6bd0a4ff5185de0154ea8081f
a-azarian <zonubi@yahoo.com>
2025-05-23 22:41:55 +0330

    Update your-photoswipe-script.js

--- your-photoswipe-script.js
+++ your-photoswipe-script.js
@@ -1,19 +1,33 @@
-import PhotoSwipeLightbox from './photoswipe-lightbox.js';
+import PhotoSwipeLightbox from '/photoswipe/photoswipe-lightbox.esm.js';
 
+// Simple fullscreen API
 const fullscreenAPI = getFullscreenAPI();
 
+// Create custom container
+// which will be stretched to fullscreen.
+//
+// (we can not use PhotoSwipe root element (.pswp),
+//  as it is created only after openPromise is resolved)
+//
 const pswpContainer = getContainer();
 
 function getFullscreenPromise() {
+  // Always resolve promise,
+  // as wa want to open lightbox 
+  // (no matter if fullscreen is supported or not)
   return new Promise((resolve) => {
     if (!fullscreenAPI || fullscreenAPI.isFullscreen()) {
+      // fullscreen API not supported, or already fullscreen
       resolve();
       return;
     }
 
-    document.addEventListener(fullscreenAPI.change, () => {
+    document.addEventListener(fullscreenAPI.change, (event) => {
       pswpContainer.style.display = 'block';
-      setTimeout(() => resolve(), 300);
+      // delay to make sure that browser fullscreen animation is finished
+      setTimeout(function() {
+        resolve();
+      }, 300);
     }, { once: true });
 
     fullscreenAPI.request(pswpContainer);
@@ -23,25 +37,40 @@ function getFullscreenPromise() {
 const lightbox = new PhotoSwipeLightbox({
   gallery: '#gallery--native-fs',
   children: 'a',
-  pswpModule: () => import('./photoswipe.esm.js'),
+  pswpModule: () => import('/photoswipe/photoswipe.esm.js'),
+
+  // Add function that returns promise
   openPromise: getFullscreenPromise,
+  
+  // Append PhotoSwipe to our container
   appendToEl: fullscreenAPI ? pswpContainer : document.body,
+
+  // disable opening/closing animations
   showAnimationDuration: 0,
   hideAnimationDuration: 0,
+
+  // Add if you're using responsive images
+  // since viewport size is unpredictable
+  // at initialization
   preloadFirstSlide: false
 });
-
 lightbox.on('close', () => {
   pswpContainer.style.display = 'none';
   if (fullscreenAPI && fullscreenAPI.isFullscreen()) {
     fullscreenAPI.exit();
   }
 });
-
 lightbox.init();
 
+// Simple fullscreen API helper,
+// supports unprefixed and webkit-prefixed versions
 function getFullscreenAPI() {
-  let enterFS, exitFS, elementFS, changeEvent, errorEvent;
+  let api;
+  let enterFS;
+  let exitFS;
+  let elementFS;
+  let changeEvent;
+  let errorEvent;
 
   if (document.documentElement.requestFullscreen) {
     enterFS = 'requestFullscreen';
@@ -58,66 +87,37 @@ function getFullscreenAPI() {
   }
 
   if (enterFS) {
-    return {
-      request(el) {
+    api = {
+      request: function (el) {
         if (enterFS === 'webkitRequestFullscreen') {
           el[enterFS](Element.ALLOW_KEYBOARD_INPUT);
         } else {
           el[enterFS]();
         }
       },
-      exit() {
+
+      exit: function () {
         return document[exitFS]();
       },
-      isFullscreen() {
+
+      isFullscreen: function () {
         return document[elementFS];
       },
+
       change: changeEvent,
       error: errorEvent
     };
   }
 
-  return null;
-}
+  return api;
+};
 
 function getContainer() {
-  const container = document.createElement('div');
-  container.style.background = '#000';
-  container.style.width = '100%';
-  container.style.height = '100%';
-  container.style.display = 'none';
-  document.body.appendChild(container);
-  return container;
+  const pswpContainer = document.createElement('div');
+  pswpContainer.style.background = '#000';
+  pswpContainer.style.width = '100%';
+  pswpContainer.style.height = '100%';
+  pswpContainer.style.display = 'none';
+  document.body.appendChild(pswpContainer);
+  return pswpContainer;
 }
-function openFullscreen() {
-  const elem = document.documentElement;
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-
-function handleOrientationChange() {
-  // اگر حالت گوشی افقی شد (landscape)
-  if(window.matchMedia("(orientation: landscape)").matches) {
-    openFullscreen();
-  } else {
-    // اگر حالت عمودی شد و فول‌اسکرین بود، خارج شو
-    if (document.fullscreenElement || document.webkitFullscreenElement) {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      }
-    }
-  }
-}
-
-// هنگام بارگذاری صفحه
-window.addEventListener('load', handleOrientationChange);
-// هنگام تغییر جهت صفحه
-window.addEventListener('orientationchange', handleOrientationChange);
-window.addEventListener('resize', handleOrientationChange);
